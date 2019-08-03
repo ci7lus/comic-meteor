01a3fa4250a17640d06d6fafe8578d3513f15c2f
ci7lus <7887955+ci7lus@users.noreply.github.com>
2019-08-03 16:08:44 +0900

    add index page

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,19 @@
 import "reflect-metadata"
 import Koa from "koa"
+import Router from "koa-router"
 import rssRouter from "./meteor"
+import { createReadStream } from "fs"
 
 const app = async () => {
     const app = new Koa()
 
+    const router = new Router<any, any>()
+    router.get("/", async ctx => {
+        ctx.body = createReadStream(`${__dirname}/index.html`)
+        ctx.type = "html"
+    })
+
+    app.use(router.routes())
     app.use(rssRouter.routes())
 
     const port = parseInt(process.env.PORT!) || 5000
